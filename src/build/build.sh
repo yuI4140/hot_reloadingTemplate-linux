073a7bd443c59fbde5782ac96b9c74ad1a262149
yuI4140 <102521443+yuI4140@users.noreply.github.com>
2023-07-27 21:09:01 +0200

    little fix

--- src/build/build.sh
+++
@@ -1,5 +0,0 @@
-set -xe
-gcc -ggdb -fPIC -shared -o ./lib/mylib.so ./src/myLib.c
-gcc -ggdb -L ./lib/ -o bin/main src/main.c src/loader.c -ldl
-LD_LIBRARY_PATH="./lib/*" ./bin/main
-#x86_64-w64-mingw32-gcc -Wall -Wextra -Werror -I./headers -o ./bin/main ./src/main.c
